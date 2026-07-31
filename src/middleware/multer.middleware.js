7c9f09c09a562dd4dac4dddc80eca9443e82db51
suhani singhal <144513906+suhani821@users.noreply.github.com>
2026-07-31 11:15:34 +0000

    user to server file storing

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,12 @@
+//to store file uploaded by user in local disk server;
+
+import multer from "multer";
+const storage = multer.diskStorage({
+    destination: function(req, file ,cb){
+        cb(null, "./public/temp")
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})
+export const upload =multer({storage,})k
