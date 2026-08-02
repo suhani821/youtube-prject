499bb4d343e953552085df82b05fc6a7dc639eb3
suhani singhal <144513906+suhani821@users.noreply.github.com>
2026-08-02 16:26:28 +0000

    getting response from user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,27 @@
 import asyncHandler from "../utils/asynchandler.js";
 const registerUser = asyncHandler( async( req, res)=>{
-res.status(200).json({
-    message:"working"
-})
+    //TO REGISTER USER
+
+
+    //user details
+    //check if user send correct and are not null
+    //check if user already exist :username, email
+    //check for images :avatar
+    //upload them to cloudnary
+    //if uploaded correctly
+    //create user entry in database
+    //remove password and refresh tokens from response
+    //check for user creation
+    //return response
+
+
+
+    
+
+
+    //data coming from form or json ( comes inside req.body)
+const {username,email, password,fullname            }=req.body
 
 })
+
 export {registerUser};
